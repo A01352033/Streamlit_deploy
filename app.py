79c81e188fe4d73494e6e1585a2e515353c7f66b
Manuel Villalpando Linares <manuvilla01@hotmail.com>
2025-05-16 09:32:45 -0600

    pendeja

--- app.py
+++ app.py
@@ -1,17 +1,51 @@
 import streamlit as st
 
-st.title("Hello, world!")
-st.write("This is a simple Streamlit app.")
-st.text_input("Enter your name:", "")
-st.text_area("Enter your message:", "")
-st.button("Submit")
+# Web page to tell my couple I love her 
+st.set_page_config(page_title="Anaí pendeja", page_icon="❤️")
+st.title("Anaí pendeja")
+st.write("Tkm")
 
-# Add a sidebar
-st.sidebar.title("Sidebar")
-st.sidebar.write("This is a sidebar.")
-st.sidebar.text_input("Enter your age:", "")
-st.sidebar.text_area("Enter your address:", "")
-st.sidebar.button("Submit to Sidebar")
-st.sidebar.write("This is a sidebar button.")
-st.sidebar.write("This is a sidebar text.")
-st.sidebar.write("This is a sidebar text.")
+# Make a heart with ascii art
+st.write("""
+```
+    .-.-.  .-.-.
+     (     )(     )
+    `-.-'  `-.-'
+    """)
+# Add a button to go to the next page
+if st.button("Next"):
+    st.write("Next page")
+    st.balloons()
+# Add a button to go to the previous page   
+if st.button("Previous"):
+    st.write("Previous page")
+    st.balloons()
+# Add a button to go to the home page
+if st.button("Home"):
+    st.write("Home page")
+    st.balloons()
+# Add a button to go to the contact page
+if st.button("Contact"):
+    st.write("Contact page")
+    st.balloons()
+# Add a button to go to the about page
+if st.button("About"):
+    st.write("About page")
+    st.balloons()
+# Add a button to go to the help page
+if st.button("Help"):
+    st.write("Help page")
+    st.balloons()
+# Add a button to go to the settings page
+if st.button("Settings"):
+    st.write("Settings page")
+    st.balloons()
+# Add a button to go to the feedback page
+if st.button("Feedback"):
+    st.write("Feedback page")
+    st.balloons()
+# Add a button to go to the support page
+if st.button("Support"):
+    st.write("Support page")
+    st.balloons()
+    
