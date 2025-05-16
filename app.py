9aa6ec72f5104ad493429a1f7f8a500d0f78b4d3
Manuel Villalpando Linares <manuvilla01@hotmail.com>
2025-05-16 09:36:13 -0600

    anahi

--- app.py
+++ app.py
@@ -13,39 +13,3 @@ st.write("""
     `-.-'  `-.-'
     """)
 # Add a button to go to the next page
-if st.button("Next"):
-    st.write("Next page")
-    st.balloons()
-# Add a button to go to the previous page   
-if st.button("Previous"):
-    st.write("Previous page")
-    st.balloons()
-# Add a button to go to the home page
-if st.button("Home"):
-    st.write("Home page")
-    st.balloons()
-# Add a button to go to the contact page
-if st.button("Contact"):
-    st.write("Contact page")
-    st.balloons()
-# Add a button to go to the about page
-if st.button("About"):
-    st.write("About page")
-    st.balloons()
-# Add a button to go to the help page
-if st.button("Help"):
-    st.write("Help page")
-    st.balloons()
-# Add a button to go to the settings page
-if st.button("Settings"):
-    st.write("Settings page")
-    st.balloons()
-# Add a button to go to the feedback page
-if st.button("Feedback"):
-    st.write("Feedback page")
-    st.balloons()
-# Add a button to go to the support page
-if st.button("Support"):
-    st.write("Support page")
-    st.balloons()
-    
